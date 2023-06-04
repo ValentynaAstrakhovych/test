b5cad47408a0f887950dd1030e8ca7684e96b444
Valentyna Astrakhovych <valentynaastrakhovych@gmail.com>
2023-06-04 01:53:53 -0300

    4 june

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
-const img= document.querySelector('.first')
+const imgA= document.querySelector('.first')
 
 const allBgIm=['images/main.png', 'images/main2.jpeg', 'images/main3.webp'];
 
 function changeBg(){
-    img.style.backgroundImage="url('images/main2.jpeg')";
+    imgA.style.backgroundImage="url('images/main2.jpeg')";
 }
 
  setInterval(changeBg, 2000)
 
 function changeBg2(){
-    img.style.backgroundImage="url('images/main3.webp')";
+    imgA.style.backgroundImage="url('images/main3.webp')";
 }
 
 setInterval(changeBg2,4000)
 
 function changeBg3(){
-    img.style.backgroundImage="url('images/main4.webp')";
+    imgA.style.backgroundImage="url('images/main4.webp')";
 }
 
 setInterval(changeBg3, 6000)
 
 function changeBg4(){
-    img.style.backgroundImage="url('images/main.png')";
+    imgA.style.backgroundImage="url('images/main.png')";
 }
 
 setInterval(changeBg4,8000)
+
+/*language change*/
+
+document.querySelector("#language-en").onclick = function(){document.location='index-ru.html'};
+
+document.querySelector("#language-en-b").onclick = function(){document.location='index-ru.html'}
